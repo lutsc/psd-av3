2994af8112dc3035e16234d0192ed52a0d296330
lutsc <170535336+lutsc@users.noreply.github.com>
2025-10-24 11:13:43 -0300

    Refatorando arquiteturas

--- fsm_d.vhd
+++ fsm_d.vhd
@@ -1,8 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity fsm_d is
-port(
+entity fsm_d is port(
   i_CLR_n: in std_logic;   -- clear/reset
   i_CLK: in std_logic;     -- clock
   i_a: in std_logic;       -- input 
@@ -11,21 +10,20 @@ end entity;
 
 architecture arch_fsm_d of fsm_d is   
   type t_STATE is (s_0, s_1, s_2, s_3, s_4);    -- estados: wait, k1, k2, k3 e k4
-  signal r_STATE: t_STATE;                      -- registrador de estado
-  signal w_NEXT: t_STATE;                       -- proximo estado
+  signal r_STATE, w_NEXT: t_STATE;              -- estado atual e próximo estado
 begin
 
 -- Registrador de estados
 p_STATE: process(i_CLR_n, i_CLK) 
 begin
   if (i_CLR_n = '0') then
-    r_STATE <= s_0; 
+    r_STATE <= s_0;
   elsif (rising_edge(i_CLK)) then
     r_STATE <= w_NEXT;
   end if;
 end process;
 
--- Next state function
+-- Função de próximo estado e saída
 p_NEXT: process(r_STATE, i_a)
 begin
   case (r_STATE) is
@@ -33,35 +31,29 @@ begin
     if (i_a = '1') then 
       w_NEXT <= s_1;
     else
+      o_r <= '0';
       w_NEXT <= s_0;
     end if;
-
+    
   when s_1 =>
     o_r <= '1';
     w_NEXT <= s_2;
-
+    
   when s_2 =>
     o_r <= '1';
     w_NEXT <= s_3;
-
+  
   when s_3 =>
     o_r <= '0';
     w_NEXT <= s_4;
-
+    
   when s_4 =>
     o_r <= '1';
     w_NEXT <= s_0;
-
+    
   when others =>
     o_r <= '0';
     w_NEXT <= s_0;
-     
-  -- when s_1 => w_NEXT <= s_2;
-  -- when s_2 => w_NEXT <= s_3;
-  -- when s_3 => w_NEXT <= s_4;
-  -- when s_4 => w_NEXT <= s_0;
-  -- when others => w_NEXT <= s_0;
   end case; 
 end process;
--- o_r <= '1' when (r_STATE = s_1) else '0';
 end architecture;
--- testbench_fsm_d.vhd
+++ testbench_fsm_d.vhd
@@ -1,19 +1,81 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity testbench is
+entity testbench_fsm_d is
 end entity;
 
-architecture tb of testbench is   
+architecture arch_fsm_d of testbench_fsm_d is   
 
-component fsm_d is
-port(
+component fsm_d is port(
   i_CLR_n: in std_logic;   -- clear/reset
   i_CLK: in std_logic;     -- clock
   i_a: in std_logic;       -- input 
   o_r: out std_logic);     -- output
 end component;
 
-begin
+signal w_CLR_n, w_CLK: std_logic := '0';
+signal w_a, w_r: std_logic;
+constant c_PERIOD: time := 1 ns;
 
-end tb;
+begin
+u_DUV: fsm_d port map(
+  i_CLR_n => w_CLR_n,
+  i_CLK => w_CLK,
+  i_a => w_a,
+  o_r => w_r);
+
+-- Clock
+w_CLK <= not w_CLK after c_PERIOD/2;
+  
+p_INPUTS: process
+begin
+  -- Reset
+  w_CLR_n <= '0';
+  w_a <= '0';
+  wait for c_PERIOD;
+
+  -- Solta reset
+  w_CLR_n <= '1';
+  wait for c_PERIOD;
+  assert (w_r = '0') report "Falha em s_0: o_r não resulta 0" severity error;
+
+  -- Ativa 'a' para começar
+  w_a <= '1';
+  wait for c_PERIOD;
+  assert (w_r = '1') report "Falha em s_1: o_r não resulta 1" severity error;
+  
+  -- Desativa 'a', os estados devem trocar automaticamente
+  w_a <= '0';
+  wait for c_PERIOD;
+  assert (w_r = '1') report "Falha em s_2: o_r não resulta 1" severity error;
+  
+  wait for c_PERIOD;
+  assert (w_r = '0') report "Falha em s_3: o_r não resulta 0" severity error;
+  
+  wait for c_PERIOD;
+  assert (w_r = '1') report "Falha em s_4: o_r não resulta 1" severity error;
+  
+  wait for c_PERIOD;
+  assert (w_r = '0') report "Falha em s_0: o_r não resulta 0" severity error;
+
+  -- Mantém 'a' ativado durante as trocas, deve manter os mesmos resultados anteriores 
+  w_a <= '1';
+  wait for c_PERIOD;
+  assert (w_r = '1') report "Falha em s_2: o_r não resulta 1" severity error;
+  
+  wait for c_PERIOD;
+  assert (w_r = '0') report "Falha em s_3: o_r não resulta 0" severity error;
+  
+  wait for c_PERIOD;
+  assert (w_r = '1') report "Falha em s_4: o_r não resulta 1" severity error;
+  
+  wait for c_PERIOD;
+  assert (w_r = '0') report "Falha em s_0: o_r não resulta 0" severity error;
+
+  -- Zerando entradas
+  w_a <= '0';
+  
+  assert false report "Teste Feito." severity note;
+  wait;
+  end process;
+end architecture;
