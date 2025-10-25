cfec730c864c7facc1afa927657e778e570b29d5
lutsc <170535336+lutsc@users.noreply.github.com>
2025-10-25 16:33:15 -0300

    Testbench do método direto feito

--- fsm_h.vhd
+++ fsm_h.vhd
@@ -1,18 +1,38 @@
+-- FSM de forma hierárquica
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity fsm_h is
-port(
+entity fsm_h is port(
   i_CLR_n: in std_logic;   -- clear/reset
-  i_CLK: in std_logic;     -- clock
-  i_a: in std_logic;       -- input 
-  o_r: out std_logic);     -- output
+  i_CLK:   in std_logic;   -- clock
+  i_a:     in std_logic;   -- input 
+  o_r:     out std_logic); -- output
 end entity;
 
 architecture arch_fsm_h of fsm_h is
-  type t_STATE is (s_0, s_1, s_2, s_3, s_4);    -- estados: wait, k1, k2, k3 e k4
-  signal r_STATE: t_STATE;                      -- registrador de estado
-  signal w_NEXT: t_STATE;                       -- proximo estado
+
+-- s_0 até s_4, um estado pra cada OU s_0 até s_2, usando lógica de bits (000, 001, 010, 011, 100) (?) 
+-- possível até usar vetores para caso de bits
+signal s_atual, s_prox: std_logic_vector(2 downto 0);
+-- signal s_2, s_1, s_0: std_logic;
+-- signal n_2, n_1, n_0: std_logic;
+signal w_r: std_logic;  
+
 begin
+
+-- Registrador de estados
+p_STATE: process(i_CLR_n, i_CLK, i_a)
+begin
+  if (i_CLR_n = '0') then
+    o_r <= '0';
+  elsif (rising_edge(i_CLK) and i_a = '1') then
+    o_r <= '1';
+  end if;
+end process;
+
+  -- n2 <= s1 and i_a;
+  -- n1 <= s1 xor s0;
+  -- n0 <= s1 and s0;
+  -- o_r <= s2 xor (s1 or s0)';
 
 end architecture;
--- testbench_fsm_d.vhd
+++ testbench_fsm_d.vhd
@@ -4,75 +4,80 @@ use ieee.std_logic_1164.all;
 entity testbench_fsm_d is
 end entity;
 
-architecture arch_fsm_d of testbench_fsm_d is   
+architecture arch_testbench_fsm_d of testbench_fsm_d is   
 
 component fsm_d is port(
   i_CLR_n: in std_logic;   -- clear/reset
-  i_CLK: in std_logic;     -- clock
-  i_a: in std_logic;       -- input 
-  o_r: out std_logic);     -- output
+  i_CLK:   in std_logic;   -- clock
+  i_a:     in std_logic;   -- input 
+  o_r:     out std_logic); -- output
 end component;
 
-signal w_CLR_n, w_CLK: std_logic := '0';
-signal w_a, w_r: std_logic;
-constant c_PERIOD: time := 1 ns;
+signal w_CLR_n, w_CLK: std_logic := '0'; -- Fios para controller e clock
+signal w_a, w_r: std_logic;              -- Fios para entrada e saída
+constant c_PERIOD: time := 1 ns;         -- Período de tempo constante para o clock 
 
 begin
-u_DUV: fsm_d port map(
-  i_CLR_n => w_CLR_n,
-  i_CLK => w_CLK,
-  i_a => w_a,
-  o_r => w_r);
 
--- Clock
+u_DUT: fsm_d port map(
+  i_CLR_n => w_CLR_n,
+  i_CLK =>   w_CLK,
+  i_a =>     w_a,
+  o_r =>     w_r);
+
+-- Sinais para Clock e Clear/Reset
 w_CLK <= not w_CLK after c_PERIOD/2;
-  
+w_CLR_n <= '1' after c_PERIOD/2;
+
 p_INPUTS: process
 begin
-  -- Reset
+  -- Clear/Reset
   w_CLR_n <= '0';
   w_a <= '0';
   wait for c_PERIOD;
-
-  -- Solta reset
+  
+  -- Solta Clear/Reset
   w_CLR_n <= '1';
   wait for c_PERIOD;
-  assert (w_r = '0') report "Falha em s_0: o_r não resulta 0" severity error;
+  assert (w_r = '0') report "Falha em Wait: o_r não resultou 0" severity error;
 
-  -- Ativa 'a' para começar
+  -- Teste com 'a' pressionado apenas apenas por
   w_a <= '1';
   wait for c_PERIOD;
-  assert (w_r = '1') report "Falha em s_1: o_r não resulta 1" severity error;
-  
-  -- Desativa 'a', os estados devem trocar automaticamente
   w_a <= '0';
-  wait for c_PERIOD;
-  assert (w_r = '1') report "Falha em s_2: o_r não resulta 1" severity error;
+  assert (w_r = '1') report "Falha em K1: o_r não resultou 1" severity error;
   
   wait for c_PERIOD;
-  assert (w_r = '0') report "Falha em s_3: o_r não resulta 0" severity error;
-  
-  wait for c_PERIOD;
-  assert (w_r = '1') report "Falha em s_4: o_r não resulta 1" severity error;
-  
-  wait for c_PERIOD;
-  assert (w_r = '0') report "Falha em s_0: o_r não resulta 0" severity error;
+  assert (w_r = '1') report "Falha em K2: o_r não resultou 1" severity error;
 
-  -- Mantém 'a' ativado durante as trocas, deve manter os mesmos resultados anteriores 
+  wait for c_PERIOD;
+  assert (w_r = '0') report "Falha em K3: o_r não resultou 0" severity error;
+  
+  wait for c_PERIOD;
+  assert (w_r = '1') report "Falha em K4: o_r não resultou 1" severity error;
+  
+  wait for c_PERIOD;
+  assert (w_r = '0') report "Falha em Wait: o_r não resultou 0" severity error;
+
+  -- Teste com 'a' ativado durante todo processo, deve manter os mesmos resultados que antes 
   w_a <= '1';
   wait for c_PERIOD;
-  assert (w_r = '1') report "Falha em s_2: o_r não resulta 1" severity error;
+  assert (w_r = '1') report "Falha em K1: o_r não resultou 1" severity error;
+
+  wait for c_PERIOD;
+  assert (w_r = '1') report "Falha em K2: o_r não resultou 1" severity error;
   
   wait for c_PERIOD;
-  assert (w_r = '0') report "Falha em s_3: o_r não resulta 0" severity error;
+  assert (w_r = '0') report "Falha em K3: o_r não resultou 0" severity error;
   
   wait for c_PERIOD;
-  assert (w_r = '1') report "Falha em s_4: o_r não resulta 1" severity error;
+  assert (w_r = '1') report "Falha em K4: o_r não resultou 1" severity error;
   
   wait for c_PERIOD;
-  assert (w_r = '0') report "Falha em s_0: o_r não resulta 0" severity error;
+  assert (w_r = '0') report "Falha em Wait: o_r não resultou 0" severity error;
 
   -- Zerando entradas
+  w_CLR_n <= '0';
   w_a <= '0';
   
   assert false report "Teste Feito." severity note;
