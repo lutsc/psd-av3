6cfd6354d4362040ae18ad46196592f0863e3ccf
lutsc <170535336+lutsc@users.noreply.github.com>
2025-10-31 15:59:42 -0300

    Atualizando FSM Hierárquico

--- fsm_h.vhd
+++ fsm_h.vhd
@@ -5,29 +5,16 @@ use ieee.std_logic_1164.all;
 entity fsm_h is port(
   i_CLR_n: in std_logic;   -- clear/reset
   i_CLK:   in std_logic;   -- clock
-  i_a:     in std_logic;   -- input 
+  i_a:     in std_logic;   -- input
   o_r:     out std_logic); -- output
 end entity;
 
 architecture arch_fsm_h of fsm_h is
-
-  component fsm_h_next is port(
-    i_a: in std_logic;
-    i_STATE: in std_logic_vector(2 downto 0);
-    o_NEXT: out std_logic_vector(2 downto 0));
-  end component;
-
-  component fsm_h_output is port(
-    i_STATE: in std_logic_vector(2 downto 0);
-    o_r:     out std_logic);
-  end component;
-
-  signal r_STATE, w_NEXT: std_logic_vector(2 downto 0);
-
+  signal r_STATE, w_NEXT: std_logic_vector(2 downto 0); -- estado atual e próximo estado
 begin
 
 -- Registrador de estados
-process(i_CLR_n, i_CLK)
+p_STATE: process(i_CLR_n, i_CLK)
 begin
   if (i_CLR_n = '0') then
     r_STATE <= "000";
@@ -36,15 +23,11 @@ begin
   end if;
 end process;
 
--- -- Transição de estados
-u_NEXT: fsm_h_next port map (i_a, r_STATE, w_NEXT);
+-- Circuito Combinacional
+w_NEXT(0) <= (not r_STATE(2) and not r_STATE(0)) and ((not r_STATE(1) and i_a) or r_STATE(1));
+w_NEXT(1) <= not r_STATE(2) and (r_STATE(1) xor r_STATE(0));
+w_NEXT(2) <= not r_STATE(2) and r_STATE(1) and r_STATE(0);
 
--- w_NEXT(0) <= (not r_STATE(2) and not r_STATE(0)) and ((not r_STATE(1) and i_a) or r_STATE(1));
--- w_NEXT(1) <= not r_STATE(2) and (r_STATE(1) xor r_STATE(0));
--- w_NEXT(2) <= not r_STATE(2) and r_STATE(1) and r_STATE(0);
-
--- Saída
-u_OUTPUT: fsm_h_output port map (r_STATE, o_r);
--- o_r <= (not r_STATE(2) and (r_STATE(1) xor r_STATE(0))) or (r_STATE(2) and not r_STATE(1) and not r_STATE(0));
+o_r <= (not r_STATE(2) and (r_STATE(1) xor r_STATE(0))) or (r_STATE(2) and not r_STATE(1) and not r_STATE(0));
 
 end architecture;
